907e660437ad743445e1dad86fcedab840db90a2
CGM <rmsauddhkdwk@gmail.com>
2022-03-15 17:23:54 +0900

    타입스크립트 나의 실수를 예방가능하며 특정 변수는 선택적 변수로 끝에 ? 를 달아 기존 자바스크립트와 같이 실행가능하게 변경 가능함

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "cgm", age = 26, gender = "male";
+const sayHi = (name, age, gender) => {
+    if (gender === undefined) {
+        gender = "성별은 알려주고 싶지 않";
+    }
+    console.log(`안녕! ${name}, 너의 나이는 ${age}이고 ${gender}구나!`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
