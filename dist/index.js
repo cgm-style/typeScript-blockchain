b277cdea4b7373171365c627f35319b7ce120683
CGM <rmsauddhkdwk@gmail.com>
2022-03-16 11:42:12 +0900

    타입스크립트의 강력함 인터페이스

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    if (gender === undefined) {
-        gender = "성별은 알려주고 싶지 않";
-    }
-    return `안녕! ${name}, 너의 나이는 ${age}이고 ${gender}구나!!`;
+const person = {
+    name: "cgm",
+    age: 26,
+    gender: "male"
 };
-console.log(sayHi("cgm", 26, "male"));
+const sayHi = (person) => {
+    if (person.gender === undefined) {
+        person.gender = "성별은 알려주고 싶지 않";
+    }
+    return `안녕! ${person.name}, 너의 나이는 ${person.age}이고 ${person.gender}구나!!`;
+};
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-const sayHi = (name:string,age:number,gender:string):string  => {
-    if(gender === undefined) {
-        gender = "성별은 알려주고 싶지 않"
+interface human {
+    name:string,
+    age:number,
+    gender?:string  // ?를 넣어 줌으로 선택적인자가 되어 없더라도 출력이 됨
+}
+
+const person = {
+    name:"cgm",
+    age:26,
+    gender:"male"
+}
+
+const sayHi = (person:human):string  => {
+    if(person.gender === undefined) {
+        person.gender = "성별은 알려주고 싶지 않"
     }
 
-    return `안녕! ${name}, 너의 나이는 ${age}이고 ${gender}구나!!`
+    return `안녕! ${person.name}, 너의 나이는 ${person.age}이고 ${person.gender}구나!!`
 };
 
-console.log(sayHi("cgm",26,"male"));
+console.log(sayHi(person));
 
 export{};
