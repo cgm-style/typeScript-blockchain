8be2eb9b70bf047b015b1e620e99b34d3b9c97e8
CGM <rmsauddhkdwk@gmail.com>
2022-03-16 12:21:37 +0900

    interface와 class 그리고 class의 public과 praivate

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+/*interface human {     // interface는 컴파일 되었을때 js에 생성이 되지 않지만 class는 생성이 됨
+    name:string,
+    age:number,
+    gender?:string  // ?를 넣어 줌으로 선택적인자가 되어 없더라도 출력이 됨
+}
 const person = {
-    name: "cgm",
-    age: 26,
-    gender: "male"
-};
+    name:"cgm",
+    age:26,
+    gender:"male"
+}*/
+const lynn = new human("lynn", 22);
 const sayHi = (person) => {
     if (person.gender === undefined) {
         person.gender = "성별은 알려주고 싶지 않";
     }
     return `안녕! ${person.name}, 너의 나이는 ${person.age}이고 ${person.gender}구나!!`;
 };
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-interface human {
+class human { 
+    public name:string; //public은 이 클래스 외에서도 가능하나 private는 외에서 불러오는게 불가능함
+    public age:number;  
+    public gender:string;
+    constructor(name:string,age:number,gender?:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+/*interface human {     // interface는 컴파일 되었을때 js에 생성이 되지 않지만 class는 생성이 됨
     name:string,
     age:number,
     gender?:string  // ?를 넣어 줌으로 선택적인자가 되어 없더라도 출력이 됨
 }
-
 const person = {
     name:"cgm",
     age:26,
     gender:"male"
-}
+}*/
+
+const lynn = new human("lynn",22);
 
 const sayHi = (person:human):string  => {
     if(person.gender === undefined) {
@@ -18,6 +30,6 @@ const sayHi = (person:human):string  => {
     return `안녕! ${person.name}, 너의 나이는 ${person.age}이고 ${person.gender}구나!!`
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export{};
